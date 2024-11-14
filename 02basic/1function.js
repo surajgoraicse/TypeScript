2a81f9f643bb7bb26e4de0a499e44b18d9c7869f
surajgoraicse <surajgoraicse@gmail.com>
2024-11-14 21:02:46 +0530

    return type

---
+++ 02basic/1function.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getString(val) {
+    return val.toString();
+}
+var value = getString(34);
+console.log(value);
+function findSquare(num) {
+    return num * num;
+}
+var sq = findSquare(34);
+var login = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+login("suraj", "suraj@gorai.com");
---
+++ 02basic/1function.ts
@@ -0,0 +1,27 @@
+function getString(val: any) {
+    return val.toString();
+}
+
+const value = getString(34);
+console.log(value);
+
+
+function findSquare(num: number) {
+    return num * num;
+}
+const sq = findSquare(34)
+
+
+const login = (name: string , email: string, isPaid:boolean = false) => {
+    
+}
+
+login("suraj" , "suraj@gorai.com" )
+
+
+
+
+
+
+
+export {}
