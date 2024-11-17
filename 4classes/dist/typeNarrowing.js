b1414e2f1107e673de970e981ff84eb435398fcd
surajgoraicse <surajgoraicse@gmail.com>
2024-11-17 21:02:34 +0530

    type narrowing

--- 4classes/dist/typeNarrowing.js
+++ 4classes/dist/typeNarrowing.js
@@ -1,7 +1,40 @@
 "use strict";
 function fn(val) {
     if (typeof val === "string") {
-        return val.toUpperCase();
+        console.log(val.toUpperCase());
+        return;
     }
-    return val.toFixed;
+    console.log(val.toFixed());
+    ;
+}
+function fn1(val) {
+    if (!val) {
+        console.log("please provide a value");
+    }
+    val === null || val === void 0 ? void 0 : val.toUpperCase();
+}
+// function to check whether object is admin or not
+function isAdminAccount(account) {
+    // account.isAdmin (not a valid statement as isAdmin is only in Admin)
+    // type narrowing
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
+// fn to check if pet is fish (will not work)
+function isFish1(pet) {
+    return pet.swim !== undefined;
+}
+// check if pet is fish
+function isFish2(pet) {
+    return pet.swim !== undefined;
+}
+// this get food will not work. 
+function getFood1(pet) {
+    if (isFish2(pet)) {
+        return "fish food";
+    }
+    else {
+        return "bird food";
+    }
 }
--- 4classes/src/typeNarrowing.ts
+++ 4classes/src/typeNarrowing.ts
@@ -1,8 +1,65 @@
 
 
-function fn(val: number | string) {
+function fn(val: number | string): void {
     if (typeof val === "string") {
-        return val.toUpperCase();
+        console.log(val.toUpperCase());
+        return;
     }
-    return val.toFixed;
+    console.log(val.toFixed());;
 }
+
+function fn1(val: string | null): void{
+    if (!val) {
+        console.log("please provide a value");
+    }
+    val?.toUpperCase();
+}
+
+
+interface User{
+    name: string,
+    email: string
+}
+
+interface Admin{
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+// function to check whether object is admin or not
+
+function isAdminAccount(account: User | Admin) {
+    // account.isAdmin (not a valid statement as isAdmin is only in Admin)
+    // type narrowing
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+}
+
+// define type
+type Fish = {
+    swim():void
+}
+type Bird = {
+    fly():void
+}
+
+// fn to check if pet is fish (will not work)
+function isFish1(pet: Fish | Bird) {
+    return   (pet as Fish).swim !== undefined 
+}
+// check if pet is fish
+function isFish2(pet: Fish | Bird): pet is Fish  {
+    return   (pet as Fish).swim !== undefined 
+}
+
+// this get food will not work. 
+function getFood1(pet: Fish | Bird) {
+    if (isFish2(pet)) {
+        return "fish food"
+    } else {
+        return "bird food"
+    }
+}
+
