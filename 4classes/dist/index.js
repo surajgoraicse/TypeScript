13a7d27726bdf8d97bcc7f27a64130df9a9c05da
surajgoraicse <surajgoraicse@gmail.com>
2024-11-17 08:32:51 +0530

    classes

--- 4classes/dist/index.js
+++ 4classes/dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("hello world");
+class Users {
+    constructor(name, pass) {
+        this.name = "";
+        this.username = name;
+        this.password = pass;
+    }
+}
+const suraj = new Users("suraj", "id3457");
--- 4classes/src/index.ts
+++ 4classes/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+
+class Users{
+
+    name: string = "";
+    username: string;
+    readonly password: string;
+    constructor(name: string , pass: string) {
+        this.username = name;
+        this.password = pass;
+    }
+}
+
+const suraj = new Users("suraj"  , "id3457")
